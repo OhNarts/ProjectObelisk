2e37fbb4d686122ddf996b21259314232f00a8fd
OhNarts <46729536+OhNarts@users.noreply.github.com>
2022-12-18 03:50:31 -0500

    Actually added all the stuff

---
+++ Assets/Scripts/Utils/ParameterUnityEvent.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class UnityEventObjectObject : UnityEvent<System.Object, System.Object> { }
+
+[Serializable]
+public class UnityEventFloat : UnityEvent<float> { }
+
+[Serializable]
+public class UnityEventInt : UnityEvent<int> { }
+
+[Serializable]
+public class UnityEventDamage : UnityEvent<DamageInfo> { }
+
---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum WeaponType { Gun, Melee }
+public enum AmmoType { Rifle, Pistol, Energy, NONE }
+
+public abstract class Weapon : MonoBehaviour
+{
+
+    [Header("Attack Point")]
+    // Where the attack comes from (i.e. where bullets come from or where melee hits) 
+    [SerializeField] protected Transform _attackPoint;
+
+    [Header("Weapon Info")]
+    // The damage an attack does
+    [SerializeField] protected float _damage;
+    [SerializeField] protected WeaponType _weaponType;
+
+    [Header("Ammo Costs/Types")]
+    [SerializeField] protected int _ammoCost1;
+    [SerializeField] protected AmmoType _ammoType1;
+
+    [SerializeField] protected int _ammoCost2;
+    [SerializeField] protected AmmoType _ammoType2;
+
+    protected PlayerController _holder = null;
+    public PlayerController holder { 
+        get { return _holder; } 
+        set { _holder = value; }
+    }
+
+
+    // Abstract methods, override these to get a weapon to fire
+    // Pass the ammo dictionary so that the methods can subtract the correct ammo amount when used
+
+    public virtual void Fire1(Dictionary<AmmoType, int> ammo) { }
+    public virtual void Fire1Stop(Dictionary<AmmoType, int> ammo) { }
+
+    public virtual void Fire2(Dictionary<AmmoType, int> ammo) { }
+    public virtual void Fire2Stop(Dictionary<AmmoType, int> ammo) { }
+}
+
