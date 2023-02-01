e1ad90e4baab3073a592413d163ebc7262d6fc79
rcarver3 <113489446+rcarver3@users.noreply.github.com>
2023-01-31 19:12:28 -0500

    main menu, options menu, and volume slider
    Very bare bones, but can be built on. added an audio mixer to allow volume to be controlled, and made a new scene for the menu. all three buttons work (for now). chose fun colors for the text for it to match, but im guessing art team will probablt change that.

---
+++ Assets/Scripts/UI/MainMenu/SettingsMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
