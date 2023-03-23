71afa5b1682061ed6e687680b9811ba051d24fe7
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-23 17:51:02 -0400

    Began working on turret

---
+++ Assets/Scripts/Enemy/TurretController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretController : EnemyController
+{
+    private EnemyState currState;
+    private NavMeshAgent agent;
+    private AmmoDictionary ammo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
