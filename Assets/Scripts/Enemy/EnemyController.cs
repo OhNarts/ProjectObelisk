4f4089ffb8aeed73cd7ee8dd1234ffa80c54b92b
Blizzard42 <46466600+Blizzard42@users.noreply.github.com>
2023-03-25 15:17:06 -0400

    Enemy Health Bars
    Enemies Prefabs now contain health bar canvases and a custom slider designed to display their current health value.
    
    After first getting damaged, the health bar is notified through the "onHealthChanged" event and appears over the enemies head indicating their current health.
    
    The health bar is a child of each enemy, but permanently rotates to face the camera.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public enum EnemyState { Idle, Chase, Attack, Stunned }
 public class EnemyController : MonoBehaviour
@@ -12,8 +13,11 @@ public class EnemyController : MonoBehaviour
     private NavMeshAgent agent;
     private AmmoDictionary ammo;
     private float stun;
-
     
+    [SerializeField] private HealthHandler healthHandler;
+    [SerializeField] private GameObject healthBar;
+    private Camera mainCamera;
+
     [SerializeField] private float distToAttack;
     [SerializeField] private Weapon weapon; public Weapon EquippedWeapon {get => weapon;}
     [SerializeField] private Transform equipPos;
@@ -33,6 +37,8 @@ public class EnemyController : MonoBehaviour
         currState = EnemyState.Idle;
         agent = transform.GetComponent<NavMeshAgent>();
         weapon.PickUpWeapon(gameObject, equipPos);
+        CreateHealthBar();
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -63,6 +69,10 @@ public class EnemyController : MonoBehaviour
                     currState = EnemyState.Idle;
                 }
                 break;
+        }
+        // Fix Health Bar Direction
+        if (healthBar.activeSelf) {
+            healthBar.transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
         }
     }
 
@@ -104,5 +114,24 @@ public class EnemyController : MonoBehaviour
 
         // Temp, can make ragdoll here instead of destroy
         Destroy(gameObject);
+    }
+
+    private void CreateHealthBar() {
+        if (healthBar == null) return;
+        /* if (healthBar.TryGetComponent<FaceCamera>(out FaceCamera faceCamera)) {
+            faceCamera = GameObject.FindWithTag("MainCamera");
+        } */
+        if (healthHandler != null && healthHandler.Health < healthHandler.MaxHealth) {
+            healthBar.SetActive(true);
+        } else {
+            healthBar.SetActive(false);
+        }
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar() {
+        if (healthHandler == null || healthBar == null) return;
+        if (healthHandler.Health < healthHandler.MaxHealth) healthBar.SetActive(true);
+        healthBar.GetComponentInChildren<Slider>(true).value = healthHandler.Health / healthHandler.MaxHealth;
     }
 }
