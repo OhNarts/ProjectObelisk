48a1762993b9f73ddccfb01d2370d29a459ad9b2
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-25 13:49:48 -0400

    Update to turret behavior

--- Assets/Scripts/Enemy/TurretController.cs
+++ Assets/Scripts/Enemy/TurretController.cs
@@ -1,23 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class TurretController : EnemyController
+
+public class TurretController : MonoBehaviour
 {
     private EnemyState currState;
-    private NavMeshAgent agent;
-    private AmmoDictionary ammo;
+    private float stun;
+    [SerializeField] Transform _target;
+    [SerializeField] private float distToAttack;
+    [SerializeField] private float fireRate;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Weapon head;
+
+    public Transform Target
     {
-        
+        set
+        {
+            _target = value;
+        }
+    }
+
+    private void Awake()
+    {
+        currState = EnemyState.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.Paused) return;
+        switch (currState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                if (Vector3.Distance(_target.position, transform.position) < distToAttack)
+                {
+                    currState = EnemyState.Attack;
+                }
+                break;
+            case EnemyState.Attack:
+                Attack();
+                if (Vector3.Distance(_target.position, transform.position) > distToAttack)
+                    currState = EnemyState.Idle;
+                break;
+        }
+    }
+
+    private void Attack()
+    {
+        transform.LookAt(_target);
+        head.Fire1Start();
+    }
+
+    private void Idle()
+    {
+        head.Fire1Stop();
     }
 }
