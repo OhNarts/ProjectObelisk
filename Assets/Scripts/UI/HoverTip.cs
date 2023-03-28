8522647893514be923cdba022d64c7ed7a765112
Aden Robertson <alrob2224@gmail.com>
2023-03-28 19:51:43 -0400

    Final inventory hover update
    The ammotype tip panel follows the mouse!

--- Assets/Scripts/UI/HoverTip.cs
+++ Assets/Scripts/UI/HoverTip.cs
@@ -7,11 +7,15 @@ public class HoverTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private float timeToWait;
     private string tipToShow = "Ammo cost: ";
+    private bool flag = false;
+    private Vector2 mousePos;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("HOVERED");
-        
+
+        flag = true;
+
         StopAllCoroutines();
         StartCoroutine(StartTimer());
     }
@@ -20,21 +24,32 @@ public class HoverTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         Debug.Log("EXITED");
 
+        flag = false;
+
         StopAllCoroutines();
         HoverTipManager.OnMouseLoseFocus();
     }
 
-    private void ShowMessage() 
+    private void ShowMessage()
     {
         InventorySlot chosenSlot = gameObject.GetComponent<InventorySlot>();
         var item = chosenSlot.Weapon;
+        mousePos = Input.mousePosition;
 
-        HoverTipManager.OnMouseHover(tipToShow + item.AmmoCost1 + "<sprite name=\"" + item.AmmoType1 + "\">", Input.mousePosition);
+        HoverTipManager.OnMouseHover(tipToShow + item.AmmoCost1 + "<sprite name=\"" + item.AmmoType1 + "\">", mousePos);
     }
 
     private IEnumerator StartTimer()
     {
         yield return new WaitForSeconds(timeToWait);
         ShowMessage();
+    }
+
+    private void Update()
+    {
+        if (flag == true)
+        {
+            ShowMessage();
+        }
     }
 }
