c7e92da0ca18b8197963a955d459ceb3e833388f
Aden Robertson <alrob2224@gmail.com>
2023-05-21 11:29:42 -0400

    Added weapon names in weapon slot

--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
     [SerializeField] private Image _uiImage;
+    [SerializeField] private TextMeshProUGUI _weaponName;
+    // [SerializeField] private string _weaponName;
     private WeaponItem _weapon;
     public WeaponItem Weapon {
         set {
             _weapon = value;
             _uiImage.sprite = _weapon.Sprite;
+            if (_weapon.WeaponName == "DoubleBarrelShotgun") {
+                _weaponName.text = "DoubleBarrel";
+            } else {
+                _weaponName.text = _weapon.WeaponName;
+            }
         }
         get {
             return _weapon;
