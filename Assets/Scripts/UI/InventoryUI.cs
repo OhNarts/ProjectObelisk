9a5799d90876919c62c220d7f0f26f8b05365978
ashu-jay <ashujay2004@gmail.com>
2023-08-25 17:16:29 -0400

    Notification for new weapon
    Additional box above health bar that gives a small description of a new weapon you pick. Also color-coded to ammo types.

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private GameObject[] _inventorySlots;
     private int _currSlot;
+    [SerializeField] private GameObject wepNotif;
+
     void Start() {
         InitializeSlots();
         PlayerState.OnPlayerStateRevert += OnPlayerStateRevert;
+        wepNotif.SetActive(false);
     }
 
     void OnEnable() {
@@ -28,6 +32,11 @@ public class InventoryUI : MonoBehaviour
         GameObject slot = _inventorySlots[_currSlot++];
         slot.GetComponent<InventorySlot>().Weapon = weapon;
         slot.SetActive(true);
+        
+        TextMeshProUGUI notifText = wepNotif.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        notifText.text = ChangeText(weapon.WeaponName);
+        notifText.color = weapon.Color;
+        wepNotif.SetActive(true);
     }
 
     private void OnPlayerStateRevert(object sender, OnPlayerStateRevertArgs e) {
@@ -50,5 +59,26 @@ public class InventoryUI : MonoBehaviour
             slot.GetComponent<InventorySlot>().Weapon = weapon;
             slot.SetActive(true);
         }
+    }
+
+    private string ChangeText(string weaponName) {
+        string finalText;
+        switch(weaponName) {
+            case "Pistol": 
+                finalText = "NEW WEAPON - Pistol!\n\nShoots one bullet at a time.\nAMMO: 10 ammo\nDAMAGE: Normal"; break;
+            case "ShotGun": 
+                finalText = "NEW WEAPON - Shotgun!\n\nShoots three bullets at a time.\nAMMO: 5 ammo\nDAMAGE: High"; break;
+            case "SMG": 
+                finalText = "NEW WEAPON - SMG!\n\nShoots bullets when mouse held.\nAMMO: 20 ammo\nDAMAGE: Low"; break;
+            case "ShockTrap":
+                finalText = "NEW GADGET - Shock Trap!\n\nStuns a nearby area of enemies when shot at.\nAMMO: 5 ammo\nDAMAGE: None"; break;
+            case "Railgun": 
+                finalText = "NEW WEAPON - Railgun!\n\nShoots one massive bullet that goes through walls.\nAMMO: 5 ammo\nDAMAGE: High"; break;
+            case "DoubleBarrelShotgun": 
+                finalText = "NEW WEAPON - Double Barrel Shotgun!\n\nShoots five bullets at a time.\nAMMO: 2 ammo\nDAMAGE: High"; break;
+            default: finalText = "Pick Weapon"; break;
+        }
+
+        return finalText;
     }
 }
